39cf15ea48ffafe81c7a5b273b6afe9fe4c02e52
Asa <26807394+a01sa01to@users.noreply.github.com>
2020-04-03 18:37:44 +0900

    Update website

--- common/script.js
+++
@@ -1,9 +0,0 @@
-// Load Common Files
-fetch("/common/common.html").then(r=>r.text()).then(t=>document.head.innerHTML += t);
-fetch("/common/header.html").then(r=>r.text()).then(t=>document.querySelector("header").innerHTML += t);
-fetch("/common/footer.html").then(r=>r.text()).then(t=>document.querySelector("footer").innerHTML += t);
-
-// Window Onload
-window.addEventListener('load',()=>{
-
-});
